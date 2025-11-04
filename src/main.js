01ece1d8feb9fe7ffe4e772256b3d798b049c77c
Thyge123 <95471753+Thyge123@users.noreply.github.com>
2025-11-04 10:28:26 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
 
-createApp(App).mount('#app')
+loadFonts();
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [],
+});
+
+createApp(App).use(vuetify).use(router).mount("#app");
